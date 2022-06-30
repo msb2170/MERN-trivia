aa360f5564314ed95fb86ed756997bb138f9a9ee
Matthew <msb2170@gmail.com>
2022-06-30 18:25:13 -0400

    routes added

---
+++ server/routes/record.js
@@ -0,0 +1,77 @@
+const { response } = require('express');
+const express = require('express');
+
+//triviaRoutes is an instance of the express router
+//we use it to define our routes
+//router will be added as middleware and will take control of requests starting with path /trivia
+
+const triviaRoutes = express.Router();
+
+//connect to the database
+const dbo = require("../db/conn");
+
+//convert id from string to ObjectId for the _id
+const ObjectId = require("mongodb").ObjectId;
+
+//Get a list of all the questions
+
+triviaRoutes.route("/trivia").get((req, res) => {
+    let db_connect = dbo.getDb("trivia");
+    db_connect
+        .collection("trivia")
+        .find({})
+        .toArray((err, result) => {
+            if (err) throw err;
+            res.json(result);
+        });
+});
+
+//Get a single question by its id
+triviaRoutes.route("/trivia/:id").get((req, res) => {
+    let db_connect = dbo.getDb();
+    let myquery = { _id: ObjectId(req.params.id) };
+    db_connect
+        .collection("trivia")
+        .findOne(myquery, (err, result) => {
+            if (err) throw err;
+            res.json(result);
+        });
+});
+
+//Add a new question
+triviaRoutes.route("/trivia/add").post((req, res) => {
+    let db_connect = dbo.getDb();
+    let myObj = {
+        question: req.body.question,
+        answer: req.body.answer,
+    };
+    db_connect.collection("trivia").insertOne(myObj, (err, res) => {
+        if (err) throw err;
+        response.json(res)
+    });
+});
+
+//Update a questions by id
+triviaRoutes.route("/update/:id").post((req, response) => {
+    let db_connect = dbo.getDb();
+    let myQuery = { _id: ObjectId(req.params.id) };
+    let newvalues = {
+        $set: {
+            question: req.body.question,
+            answer: req.body.answer
+        },
+    }
+});
+
+//delete a question
+triviaRoutes.route("/:id").delete((req, response) => {
+    let db_connect = dbo.getDb();
+    let myQuery = { _id: ObjectId(req.params.id) };
+    db_connect.collection("records").deleteOne(myQuery, (err, obj) => {
+        if (err) throw err;
+        console.log("1 document deleted");
+        response.json(obj);
+    });
+});
+
+module.exports = triviaRoutes;
