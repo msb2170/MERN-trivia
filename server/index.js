075097bddcbf2f6a816c3df64af69ad4a0cfeb6c
Matthew <msb2170@gmail.com>
2024-01-11 15:27:03 -0500

    CORS fix 2

--- server/index.js
+++ server/index.js
@@ -5,9 +5,11 @@ const cors = require('cors');
 require('dotenv').config({ path: "./config.env"});
 const port = process.env.PORT || 5000;
 app.use(cors());
-app.use(cors({
-    origin: "https://mern-trivia-one.vercel.app/"
-}));
+app.use(function (req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+ })
 const bodyParser = require("body-parser")
 
 app.use(express.json());
